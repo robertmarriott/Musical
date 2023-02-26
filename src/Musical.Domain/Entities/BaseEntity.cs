8f13d9fc3148a991e5e02560b095421dc11efc5c
Robert Marriott <marriott@telus.net>
2023-02-26 14:09:33 -0800

    Add project files.

---
+++ src/Musical.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,5 @@
+﻿namespace Musical.Domain.Entities;
+
+internal class BaseEntity
+{
+}
---
+++ src/Musical.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Musical.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureService(this IServiceCollection services)
+    {
+        return services;
+    }
+}
