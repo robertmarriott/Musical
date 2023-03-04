62bc44bb05c3c39882c7ca5876124cc44a074471
Robert Marriott <marriott@telus.net>
2023-03-03 21:30:38 -0800

    Register artist repository

--- src/Musical.Infrastructure/ServiceCollectionExtensions.cs
+++ src/Musical.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Musical.Domain.Repositories;
 using Musical.Infrastructure.Persistence;
+using Musical.Infrastructure.Persistence.Repositories;
 
 namespace Musical.Infrastructure;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<MusicalContext>(options =>
             options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IArtistRepository, ArtistRepository>();
 
         return services;
     }
